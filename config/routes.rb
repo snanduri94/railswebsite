0a40d501ae303cee58f58e7eb11a1b13e3225cef
Srikanth Nanduri <srikanthnanduri94@gmail.com>
2023-08-28 16:40:17 -0500

    Added a coming soon Games page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
-
+  get 'games' => 'pages#games'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
